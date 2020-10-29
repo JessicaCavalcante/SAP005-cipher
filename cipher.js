813012f273f63b84299263be4b35c533f7d0e629
Jessica Cavalcante <je-cavalcante@hotmail.com>
2020-10-29 20:30:31 -0300

    created function encode and decode a string

--- cipher.js
+++ cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
-  // ...
+	firstLetter: 65,
+	sizelAlphabet: 26,
+	encode: function(offset, textEncodeDecode) {
+		let sumEncode = '';
+		for(let i = 0; i < textEncodeDecode.length; i++) {
+		sumEncode += String.fromCharCode(((textEncodeDecode.charCodeAt(i) - cipher.firstLetter + offset) % cipher.sizelAlphabet) + cipher.firstLetter);
+		}
+		return sumEncode;
+
+	},
+	decode: function(offset, textEncodeDecode) {
+		let sumDecode = '';
+		for(let i = 0; i < textEncodeDecode.length; i++){
+		sumDecode += String.fromCharCode(((textEncodeDecode.charCodeAt(0) + cipher.firstLetter - offset) % cipher.sizelAlphabet) + cipher.firstLetter);
+		}
+		return sumDecode;
+	},
 };
 
 export default cipher;
