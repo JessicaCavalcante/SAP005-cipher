dc5fab61ec396ba67c02289d416156ff7564959c
Jessica Cavalcante <je-cavalcante@hotmail.com>
2020-10-29 20:31:43 -0300

    created interation between html and js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
+const textEncodeDecode = document.getElementById("text-encode-decode");
+const offset = document.getElementById("offset");
+textEncodeDecode.addEventListener("keyup", function () {upperCase(textEncodeDecode)});
+
+document.getElementById("encode").addEventListener("click", function () {encode(offset, textEncodeDecode)});
+function encode(offset, textEncodeDecode) {
+    let encode = cipher.encode(Number(offset.value),textEncodeDecode.value);
+    document.getElementById("result-encode-decode").value = encode;
+}
+
+document.getElementById("decode").addEventListener("click", function () {decode(offset, textEncodeDecode)});
+function decode(offset, textEncodeDecode) {
+    let decode = cipher.decode(Number(offset.value),textEncodeDecode.value);
+    document.getElementById("result-encode-decode").value = decode;
+}
+
+function upperCase(element) {
+    element.value = element.value.toUpperCase();
+}
+
 console.log(cipher);
