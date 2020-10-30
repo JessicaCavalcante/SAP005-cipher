4d5c26b4c12ee2508311aebd311bcff0ed6ef29b
Jessica Cavalcante <je-cavalcante@hotmail.com>
2020-10-30 19:13:45 -0300

    add focus in input textEncodeDecode and add type validation

--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@ import cipher from './cipher.js';
 
 const textEncodeDecode = document.getElementById("text-encode-decode");
 const offset = document.getElementById("offset");
+textEncodeDecode.focus();
 textEncodeDecode.addEventListener("keyup", function () {upperCase(textEncodeDecode)});
 
-document.getElementById("encode").addEventListener("click", function () {encode(offset, textEncodeDecode)});
-function encode(offset, textEncodeDecode) {
+document.getElementById("encode").addEventListener("click", function () {
     let encode = cipher.encode(Number(offset.value),textEncodeDecode.value);
     document.getElementById("result-encode-decode").value = encode;
-}
+});
 
-document.getElementById("decode").addEventListener("click", function () {decode(offset, textEncodeDecode)});
-function decode(offset, textEncodeDecode) {
+document.getElementById("decode").addEventListener("click", function () {
     let decode = cipher.decode(Number(offset.value),textEncodeDecode.value);
     document.getElementById("result-encode-decode").value = decode;
-}
+});
 
 function upperCase(element) {
     element.value = element.value.toUpperCase();
 }
-
-console.log(cipher);
