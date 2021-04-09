6f374240654944c8d8ac2f142d13c0f7434edf00
Jessica Cavalcante <je-cavalcante@hotmail.com>
2021-04-08 22:10:50 -0300

    fix indent

--- cipher.js
+++ cipher.js
@@ -1,37 +1,37 @@
 const cipher = {
-	firstLetter: 65,
-	sizelAlphabet: 26,
-	encode: function(offset, textEncodeDecode) {
-		if (!offset || Array.isArray(offset) || !textEncodeDecode || Array.isArray(textEncodeDecode)) {
-			throw new TypeError("Invalid parameters");
-		}
-		let sumEncode = '';
-		for (let i = 0; i < textEncodeDecode.length; i++) {
-			if (textEncodeDecode.charCodeAt(i) == 32) {
-				sumEncode += ' ';
-				continue;
-			}
-			sumEncode += String.fromCharCode(((textEncodeDecode.charCodeAt(i) - cipher.firstLetter + offset) % cipher.sizelAlphabet) + cipher.firstLetter);
-		}
+  firstLetter: 65,
+  sizelAlphabet: 26,
+  encode: function(offset, textEncodeDecode) {
+    if (!offset || Array.isArray(offset) || !textEncodeDecode || Array.isArray(textEncodeDecode)) {
+      throw new TypeError("Invalid parameters");
+    }
+    let sumEncode = '';
+    for (let i = 0; i < textEncodeDecode.length; i++) {
+      if (textEncodeDecode.charCodeAt(i) == 32) {
+        sumEncode += ' ';
+        continue;
+      }
+      sumEncode += String.fromCharCode(((textEncodeDecode.charCodeAt(i) - cipher.firstLetter + offset) % cipher.sizelAlphabet) + cipher.firstLetter);
+    }
 
-		return sumEncode;
+    return sumEncode;
 
-	},
-	decode: function(offset, textEncodeDecode) {
-		if (!offset || Array.isArray(offset) || !textEncodeDecode || Array.isArray(textEncodeDecode)) {
-			throw new TypeError("Invalid parameters");
-		}
-		let sumDecode = '';
-		for (let i = 0; i < textEncodeDecode.length; i++) {
-			if (textEncodeDecode.charCodeAt(i) == 32) {
-				sumDecode += ' ';
-				continue;
-			}
-			sumDecode += String.fromCharCode(((textEncodeDecode.charCodeAt(i) + cipher.firstLetter - offset) % cipher.sizelAlphabet) + cipher.firstLetter);
-		}
+  },
+  decode: function(offset, textEncodeDecode) {
+    if (!offset || Array.isArray(offset) || !textEncodeDecode || Array.isArray(textEncodeDecode)) {
+      throw new TypeError("Invalid parameters");
+    }
+    let sumDecode = '';
+    for (let i = 0; i < textEncodeDecode.length; i++) {
+      if (textEncodeDecode.charCodeAt(i) == 32) {
+        sumDecode += ' ';
+        continue;
+      }
+      sumDecode += String.fromCharCode(((textEncodeDecode.charCodeAt(i) + cipher.firstLetter - offset) % cipher.sizelAlphabet) + cipher.firstLetter);
+    }
 
-		return sumDecode;
-	},
+    return sumDecode;
+  },
 };
 
 export default cipher;
